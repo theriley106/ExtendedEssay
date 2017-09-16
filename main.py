48dcee94ed7fe1a21c9c44e6a53c7ab40c9210cc
Jagger Brulato <jaggerbrulato@dhcp-ccc-3888.eduroam.cornell.edu>
2017-09-16 14:14:25 -0400

    Different python files for each method.

--- main.py
+++ main.py
@@ -4,57 +4,7 @@ from itertools import chain
 from nltk.corpus import wordnet
 import nltk
 import sys
-from PyDictionary import PyDictionary 
-
-
-def returnComplexity(text):
-	return textstat.flesch_kincaid_grade(text)
-
-def grabSynonyms(word, typ=None):
-	if typ == None:
-		synonyms = wordnet.synsets(word)
-		a = set(chain.from_iterable([word.lemma_names() for word in synonyms]))
-		#return a
-		if len(list(a)) > 1:
-			return list(a)[0]
-	else:
-		syns = wordnet.synsets(word)
-		for i in syns:
-			text = i.examples()
-			a = findWordType(text, word)
-			if str(typ) in str(a):
-				return word
-				print 'did it'
-	return word
-def findWordType(text, word, index=0):
-	text = nltk.word_tokenize(text)
-	result = nltk.pos_tag(text)
-	result = [i for i in result if i[index].lower() == word]
-	return result
-
-def convertWordType(text):
-	converted = []
-	for word in text.split(' '):
-		if len(word) > 3:
-			try:
-				if text.split(' ').count(word) > 1:
-					index = text.split(' ').count(word) - str(converted).count(word)
-					e = findWordType(text, word, index)
-				else:
-					e = findWordType(text, word)
-				if 'VB' in e[0][1]:
-					e = grabSynonyms(e[0][0])
-				elif 'RB' in e[0][1]:
-					e = grabSynonyms(e[0][0])
-				else:
-					e = word
-			except:
-				e = word
-		else:
-			e = word
-		converted.append(e)
-	return converted
-		
+from PyDictionary import PyDictionary
 
 if __name__ == "__main__":
 	if 'debug' in str(sys.argv):
@@ -67,7 +17,7 @@ if __name__ == "__main__":
 		else:
 			a.append(word)
 	print ' '.join(a)'''
-	
+
 
 
 
