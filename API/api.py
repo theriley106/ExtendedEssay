9496a937ddc5d8b9dc2a8bf9ef747627220a6889
Theriley106 <christopherlambert106@gmail.com>
2017-09-16 14:22:54 -0400

    Added changes to master'

--- API/api.py
+++ API/api.py
@@ -1,14 +1,21 @@
-from flask import request
+from flask import Flask, request
+
+
+app = Flask(__name__)
 
 @app.route('/extendedEssay/compute', methods=['POST'])
 def create_essay():
     if not request.json or not 'title' in request.json:
         abort(400)
-    task = {
+    '''task = {
         'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
         'description': request.json.get('description', ""),
         'done': False
     }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    tasks.append(task)'''
+    print request.json
+    return request.json
+
+if __name__ == "__main__":
+    app.run()
