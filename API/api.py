be47ff06277505fb07db44634b2abc55ebd0e7f9
Theriley106 <christopherlambert106@gmail.com>
2017-09-16 14:48:46 -0400

    Added the API

--- API/api.py
+++ API/api.py
@@ -5,17 +5,10 @@ app = Flask(__name__)
 
 @app.route('/extendedEssay/compute', methods=['POST'])
 def create_essay():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    '''task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)'''
-    print request.json
-    return request.json
+    if not request.json or not 'essay' in request.json:
+       return "ERROR"
+    a = str(request.json['essay'])
+    return a
 
 if __name__ == "__main__":
     app.run()
