c55530a5633557e4342ad559d6b9b0037025cf1e
Theriley106 <christopherlambert106@gmail.com>
2017-09-16 22:30:56 -0400

    Added awesome front end stuff

--- API/api.py
+++ API/api.py
@@ -5,28 +5,37 @@ import os
 app = Flask(__name__)
 
 def doStuffToEssay(text):
-    print essay
-    return essay
+	print essay
+	return essay
 
 @app.route('/')
 def form():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/extendedEssay/compute', methods=['POST'])
 def create_essay():
-    if not request.json or not 'essay' in request.json:
-       return None
-    text = str(request.json['essay']).replace("hello", "goodbye")
-    return doStuffToEssay(text)
+	if not request.json or not 'essay' in request.json:
+	   return None
+	text = str(request.json['essay']).replace("hello", "goodbye")
+	return doStuffToEssay(text)
 
-@app.route("/downloadFile")
+
+@app.route('/test')
+def test():
+	return "Test"
+
+@app.route("/downloadFile", methods=["POST"])
 def downloadFile():
-    os.system('hello > test.txt')
-    return Response(
-        'test.txt',
-        mimetype="text",
-        headers={"Content-disposition":
-                 "attachment; filename=test.txt"})
+	#redirect(url_for('form'))
+	for key, value in request.form.items():
+		print key, value
+	os.system('hello > test.txt')
+	return Response(
+		'test.txt',
+		mimetype="text",
+		headers={"Content-disposition":
+				 "attachment; filename=test.txt"})
+	#return redirect(url_for('test'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8888)
+	app.run(host='0.0.0.0', port=8888)
